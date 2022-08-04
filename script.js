35ee2c28059e5d6432f81d42933fef80b669da10
charnchol <khumball46@gmail.com>
2022-08-04 14:10:18 +0700

    end

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+const imgprofile = document.getElementById("img-profile");
+const btnRandom = document.getElementById("btn-random");
+const imgicon = document.getElementById("span-gender-icon");
+
+if (resp.data.results.gender == "male") {
+  imgicon.innerText = "👨";
+} else if (resp.data.results.gender == "female") {
+  imgicon.innerText = "👩";
+}
+
+imgprofile.src = resp.data.results.picture.large;
